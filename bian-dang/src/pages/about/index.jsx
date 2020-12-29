0932adf24c336d2ef7366c29b1e94c730a07fbed
Jason <jasonhu00@gmail.com>
2020-12-28 20:09:26 -0800

    css cleanup

--- bian-dang/src/pages/about/index.jsx
+++ bian-dang/src/pages/about/index.jsx
@@ -9,8 +9,9 @@ export default function About() {
     <Page title="About">
       <div className={styles.header}><span className={textStyles.headerLg}>About Us</span></div>
       <div className={textStyles.bodyText}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <p>We’ve always wanted to create something that evoked a sense of home. To us, Taiwanese food is more than just a series of dishes; it’s a walking tour tasting menu of memories. Mornings hurrying past a corner food stall, a sip of comfort while growing through life stages, a common thread between mom and daughter, or a porcelain bowl cradling soupy noodles.</p>
+        <p>便當 (<em>bian dang</em>) are takeout lunch boxes, paper sandwich trays stuffed with side dishes, and they parallel our fragmented Taiwanese-American identities. Thanks for stopping by and sampling a morsel of our favorite moments, bundled together into one familiar lunch box.</p>
+        <p>開飯</p>
       </div>
     </Page>
   );
