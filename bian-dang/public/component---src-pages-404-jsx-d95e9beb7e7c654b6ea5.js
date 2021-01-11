d4bba3f970e39f99e62cf814c19e0b1fc07328ac
Jason <jasonhu00@gmail.com>
2021-01-10 22:24:45 -0800

    public

---
+++ bian-dang/public/component---src-pages-404-jsx-d95e9beb7e7c654b6ea5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{pssB:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return c}));var u=e("q1tI"),r=e.n(u);function c(){return r.a.createElement("div",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-d95e9beb7e7c654b6ea5.js.map
---
+++ bian-dang/public/component---src-pages-404-jsx-feb8449157cec9a93341.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{pssB:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return c}));var u=e("q1tI"),r=e.n(u);function c(){return r.a.createElement("div",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-feb8449157cec9a93341.js.map
