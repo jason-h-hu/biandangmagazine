f6aca9e7ac90ed3a9a9d5486245942186bf9d75b
Jason <jasonhu00@gmail.com>
2021-01-31 01:31:47 -0800

    Bugfix: More responsive team page

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {Image, Row, Col} from 'react-bootstrap'
+import {Image} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faEtsy, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 
-import textStyles from '../../css/text.module.css';
 import styles from './styles.module.css';
 
 const ICON_LOOKUP = {
@@ -16,24 +15,27 @@ export default function Profile({img, name, biography, position, social, prompt,
   return (
     <div className={styles.profile}>
       <Image className={styles.profilePicture} src={img} alt={name} fluid /> 
-      <div className={styles.profileName}>{name}</div>
-      <div className={styles.position}>{position}</div>
-      <div className={styles.prompt}>{prompt}</div>
-      <div className={styles.answer}>{answer}</div>
-      <div className={styles.socials}>{
-        social.map(({label, value, url}, i) => 
-          <a className={styles.socialLink} href={url} key={i}>
-            <span className={styles.socialLabel}>
-              {
-                ICON_LOOKUP[label] == null
-                  ? <FontAwesomeIcon icon={faGlobe} />
-                  : <FontAwesomeIcon icon={ICON_LOOKUP[label]} />
-              }
-            </span>
-            <span className={styles.socialValue} href={url}>{value}</span>
-          </a>
-        )
-      }</div>
+      <div className={styles.profileInformation}> 
+        <div className={styles.profileName}>{name}</div>
+        <div className={styles.position}>{position}</div>
+        <div className={styles.prompt}>{prompt}</div>
+        <div className={styles.answer}>{answer}</div>
+        <div className={styles.socials}>{
+          social.map(({label, value, url}, i) => 
+            <a className={styles.socialLink} href={url} key={i}>
+              <span className={styles.socialLabel}>
+                {
+                  ICON_LOOKUP[label] == null
+                    ? <FontAwesomeIcon icon={faGlobe} />
+                    : <FontAwesomeIcon icon={ICON_LOOKUP[label]} />
+                }
+              </span>
+              <span className={styles.socialValue} href={url}>{value}</span>
+            </a>
+          )
+        }</div>
+      </div>
+      
     </div>
   );
 }
