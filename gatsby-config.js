05fd0349c41623ee5bc2782ced3affd85dfa3ea1
Jason <jasonhu00@gmail.com>
2021-02-13 21:49:22 -0800

    Bugfix: Highlight :target ony for footnotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,15 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        name: 'src',
-        path: `${__dirname}/src/`,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-189685892-1",
+          "G-84LS8Q0WD1",
+        ],
       },
-    }
+    },
   ],
   pathPrefix: "/bian-dang",
 }
